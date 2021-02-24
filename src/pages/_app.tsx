e44ef42ceb151e82b47312798c74d63230328a1e
Markimg22 <marquim.net.221200@hotmail.com>
2021-02-24 18:49:46 -0400

    3º Dia: Contexto e Componentes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import '../styles/global.css';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  );
 }
